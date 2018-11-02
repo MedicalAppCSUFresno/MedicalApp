4ce3f31b7f9af470202e3f4b3a6b552dbd8f0032
pmagnuson17 <phillipmagnuson17@mail.fresnostate.edu>
2018-11-01 17:15:27 -0700

    Create account page

---
+++ HelpApp/app/src/main/java/muckmusic/gmail/com/help2/Main2Activity.java
@@ -0,0 +1,14 @@
+package muckmusic.gmail.com.help2;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+
+}
