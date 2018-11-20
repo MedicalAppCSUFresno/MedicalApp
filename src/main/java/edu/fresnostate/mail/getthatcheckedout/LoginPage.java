a48c1f328c2e8b8aeae678c19e58f9b2604c3a72
jcovarrubias11 <muckmusic11@gmail.com>
2018-11-19 18:21:42 -0800

    Re Commit

--- src/main/java/edu/fresnostate/mail/getthatcheckedout/LoginPage.java
+++ src/main/java/edu/fresnostate/mail/getthatcheckedout/LoginPage.java
@@ -9,7 +9,6 @@ import android.widget.Button;
 public class LoginPage extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
